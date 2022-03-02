9bf6b64430eeb82243ee273eac9d65ef26de9e94
Emiliano <emilianocastro97@gmail.com>
2022-03-01 22:42:22 -0300

    ✨Agregando persona model

---
+++ src/models/persona.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+import IPerson from '../interfaces/persona.interface';
+
+const PersonaSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        required: [true, 'El email es obligatoria']
+    },
+    contraseña: {
+        type: String,
+        required: [true, 'La contraseña es obligatorio']
+    },
+    telefono: {
+        type: String,
+        required: [false]
+    },
+    rol: {
+        type: String,
+        required: [true, 'El rol es obligatorio. Valores posibles: admin/cliente'],
+        enum: ['admin', 'cliente']
+    }
+}, {
+    timestamps: { createdAt: true, updatedAt: true }
+})
+
+export default model<IPerson>('Person', PersonaSchema);
