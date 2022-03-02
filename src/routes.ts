42d8d0f16c5e8d6b2b06fdd39c66566cd1148705
Emiliano <emilianocastro97@gmail.com>
2022-03-01 22:42:50 -0300

    ✨Agregando rutas de personas

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,9 @@ import { Application } from 'express';
 
 // Import routes
 import ProductosRoutes from './routes/productos.routes';
+import PersonaRoutes from './routes/persona.routes';
 
 export const registeredRoutes = async (app:Application) => {
   app.use('/api/productos', ProductosRoutes);
+  app.use('/api/personas', PersonaRoutes);
 };
