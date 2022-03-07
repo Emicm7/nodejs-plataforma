c12fb7e0a3c60ecd9309aeb87cc8e96ddf925289
Emiliano <emilianocastro97@gmail.com>
2022-03-06 21:11:37 -0300

    🔨Refactorizacion en persona routes

--- src/routes/persona.routes.ts
+++ src/routes/persona.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import * as personaController from '../controllers/persona.controller';
+import { validPersonaEmail } from '../helper/db-validator';
+import { validateFields } from '../middlewares/validate-fields';
 
 const router = Router();
 
-router.post('/', personaController.store);
+router.post('/login', personaController.login);
+router.post('/', [
+  body('email').custom(validPersonaEmail),
+  validateFields
+],personaController.store);
+
 router.get('/', personaController.index);
 router.get('/:id', personaController.show);
 router.put('/:id', personaController.update);
