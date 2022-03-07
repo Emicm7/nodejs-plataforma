08990e4e557de10cb4f0019a8f6cadb83da6d11c
Emiliano <emilianocastro97@gmail.com>
2022-03-06 21:07:52 -0300

    🔨Refactorizacion en productos controller

--- src/controllers/productos.controller.ts
+++ src/controllers/productos.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Producto from "../models/productos.model";
 import IProducto from "../interfaces/productos.interface";
+import Venta from "../models/ventas.model";
 
 // Get all resources
 export const index = async (req: Request, res: Response) => {
@@ -14,7 +15,8 @@ export const index = async (req: Request, res: Response) => {
             filters = { ...filters, nombre: {$regex: data.nombre, $options: 'i'} }
         }
 
-        let productos = await Producto.find(filters);
+        let productos = await Producto.find(filters)
+        .populate({ path: 'ventas', select: ['id', 'forma_de_pago', 'precio_total'] }); ;
 
         res.json(productos);
     } catch (error) {
@@ -66,7 +68,7 @@ export const update = async (req: Request, res: Response) => {
         let producto = await Producto.findById(id);
 
         if (!producto)
-            return res.status(404).send(`No se encontró el película con id: ${id}`);
+            return res.status(404).send(`No se encontró el producto con id: ${id}`);
         
         if (data.nombre) producto.nombre = data.nombre;
         if (data.precio) producto.precio = data.precio;
@@ -86,11 +88,76 @@ export const destroy = async (req: Request, res: Response) => {
     const id = req?.params?.id;
     try {
         let producto = await Producto.findByIdAndDelete(id);
-        console.log(producto);
+        
         if (!producto)
-            res.status(404).send(`No se encontró el película con id: ${id}`);
+            res.status(404).send(`No se encontró el producto con id: ${id}`);
         else
-            res.status(200).json(producto);
+            res.status(200).json('Producto eliminado');
+    } catch (error) {
+        res.status(500).send('Algo salió mal.');
+    }
+};
+
+export const agregarVenta = async (req: Request, res: Response) => {
+    const productoId = req?.params?.productoId;
+    const ventaId = req?.params?.ventaId;
+    try {
+        let producto = await Producto.findById(productoId);
+        let venta = await Venta.findById(ventaId);
+
+        if (!producto || !venta)
+            res.status(404).send(`No se encontró el producto o la venta indicado.`);
+        else {
+            if (producto.stock <= 0)
+                res.status(404).send(`El producto no tiene stock disponible.`);
+            await venta?.productos.push(producto?.id);
+            await producto?.ventas.push(venta?.id);
+
+            venta.precio_total += producto.precio; 
+            producto.stock--;
+            
+            await venta?.save();
+            await producto?.save();
+            res.status(201).json(producto);
+        }
+    } catch (error) {
+        res.status(500).send('Algo salió mal.');
+    }
+};
+
+export const borrarVenta = async (req: Request, res: Response) => {
+    const productoId = req?.params?.productoId;
+    const ventaId = req?.params?.ventaId;
+
+    try {
+        let producto = await Producto.findById(productoId);
+        let venta = await Venta.findById(ventaId);
+
+        if (!producto || !venta)
+            res.status(404).send(`No se encontró el producto o la venta indicado.`);
+        else {
+            // buscar info acerca de transacciones
+            let indiceVenta = Number(producto?.ventas.indexOf(ventaId));
+            let indiceProducto = Number(venta?.productos.indexOf(productoId));
+
+            if (indiceVenta === -1)
+                res.status(404).send(`No se encontró la venta en el array de ventas del producto`);
+
+            if (indiceProducto === -1)
+                res.status(404).send(`No se encontró el producto en el array de productos de la venta`);
+
+            producto?.ventas.splice(indiceVenta,1);
+            venta?.productos.splice(indiceProducto,1);
+
+            venta.precio_total -= producto.precio; 
+            producto.stock++;
+
+            await producto?.save();
+            await venta?.save();
+            
+
+            res.status(201).json(producto);
+        }
     } catch (error) {
         res.status(500).send('Algo salió mal.');
     }
