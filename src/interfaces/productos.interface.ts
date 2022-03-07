3593ca4ceff1eff9db8ce1f298feb5693418bf34
Emiliano <emilianocastro97@gmail.com>
2022-03-06 21:08:25 -0300

    🔨Refactorizacion en productos interface

--- src/interfaces/productos.interface.ts
+++ src/interfaces/productos.interface.ts
@@ -1,11 +1,13 @@
+import { ObjectId } from 'mongodb';
 import { Document } from 'mongoose'
 
 export default interface IProduct extends Document {
   _id: string;
   nombre: string;
-  precio: string;
-  stock: string;
+  precio: number;
+  stock: number;
   estado: string;
+  ventas: Array<string>;
   createdAt: Date;
   updatedAt: Date;
 };
