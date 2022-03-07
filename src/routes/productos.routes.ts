07e07acf00a355ce3b88b8e3e56b199965f042d7
Emiliano <emilianocastro97@gmail.com>
2022-03-06 21:12:09 -0300

    ✨Agregando rutas en productos

--- src/routes/productos.routes.ts
+++ src/routes/productos.routes.ts
@@ -4,9 +4,11 @@ import * as productoController from '../controllers/productos.controller';
 const router = Router();
 
 router.post('/', productoController.store);
+router.post('/:productoId/ventas/:ventaId',productoController.agregarVenta);
 router.get('/', productoController.index);
 router.get('/:id', productoController.show);
 router.put('/:id', productoController.update);
 router.delete('/:id', productoController.destroy);
+router.delete('/:productoId/ventas/:ventaId',productoController.borrarVenta);
 
 export default router;
