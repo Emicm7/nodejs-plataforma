59ce7b433c9c3f4240bab130cb9b7025f115dc8e
Emiliano <emilianocastro97@gmail.com>
2022-03-06 21:11:11 -0300

    ✨Agregando las rutas de ventas

--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,10 @@ import { Application } from 'express';
 // Import routes
 import ProductosRoutes from './routes/productos.routes';
 import PersonaRoutes from './routes/persona.routes';
+import VentasRoutes from './routes/ventas.routes';
 
 export const registeredRoutes = async (app:Application) => {
   app.use('/api/productos', ProductosRoutes);
   app.use('/api/personas', PersonaRoutes);
+  app.use('/api/ventas', VentasRoutes);
 };
